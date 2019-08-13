9293ff1a1a6a9c35d2d9ad683184357c687cf330
Rafael da Silva Lins <rdasilvalins@gmail.com>
2019-08-12 22:35:17 -0300

    upgrades

--- app.R
+++ app.R
@@ -8,13 +8,13 @@ ui <- navbarPage("Benford Analysis",
   theme = shinytheme("yeti"),
   tabPanel("English",
    sidebarLayout(
-      sidebarPanel(
+      sidebarPanel(width = 3,
          fileInput("records", strong("load dataset (.csv)"), multiple = FALSE),
-         numericInput("column", strong("column of dataset:"), 1),
-         numericInput("digits", strong("number of digits to test:"), 1),
-         numericInput("ndigits", strong('number of suspicious groups:'), 3),
-         submitButton("Apply Changes"), br(),
-         downloadButton("report", "Download report")
+         uiOutput('data_column'),
+         sliderInput("digits", strong("number of digits to test:"), 1, 4, 1),
+         fluidRow(
+           column(12, align = "center" , icon("refresh"), submitButton("Refresh"), br()),
+           column(12, align = "center" , downloadButton("report", "Download report")))
       ),
       
       mainPanel(
@@ -27,24 +27,28 @@ ui <- navbarPage("Benford Analysis",
                    ),
                    column(12, align = "center" , plotOutput('plot_digits', click = "digit_click"))
                    ),
-          tabPanel("Second Order Test",
-                   column(12,
-                          h5("The graph concerns the count for the ordered date difference")
-                   ),
-                   column(12, align = "center" , plotOutput('plot_sec_ord'))
-          ),
-          tabPanel("Summation Test",
-                   column(12,
-                          h5("The first graph concerns the deviations of summation values from expected values.")
-                   ),
-                   column(12, align = "center" , plotOutput('plot_summ_dist'))),
+          tabPanel("Formal Tests",br(),
+                   tabsetPanel(tabPanel("Second Order Test",
+                                        column(12,h5("The graph concerns the count for the ordered date difference")),
+                                        column(12, align = "center" , plotOutput('plot_sec_ord'))                 
+                                        ),
+                               tabPanel("Summation Test",
+                                        column(12, h5("The first graph concerns the deviations of summation values from expected values.")),
+                                        column(12, align = "center" , plotOutput('plot_summ_dist'))
+                                        ),
+                               tabPanel("Mantissa Arc Test",
+                                       ''
+                               )
+                               )
+                  ),
           tabPanel("Suspect Records",
+                   numericInput("ndigits", strong('number of suspicious groups:'), 3),
                    h5('leading digits by decreasing order of discrepancies (absolute differences)'),
                    column(12, align = "center" , dataTableOutput('leading_digits')),
                    h5("observations of the most suspicious groups with leading digits not complying with Benford's Law"),
                    column(12, align = "center" , dataTableOutput('suspect_records'))
           ),
-          tabPanel("More...", verbatimTextOutput('results_benf'))
+          tabPanel("More", verbatimTextOutput('results_benf'))
         )
 
       )
@@ -54,19 +58,27 @@ ui <- navbarPage("Benford Analysis",
 
 
 server <- function(input, output) {
-  
+      
   input_data <- reactive({
        if(is.null(input$records$name)){
          data("corporate.payment")
-         data <- corporate.payment$Amount
+         corporate.payment
          }else{
-       data <- read.csv(input$records$datapath, header = TRUE, sep = ";")
-       data[, input$column]}
+        as.data.frame(read.csv(input$records$datapath, header = TRUE, sep = ";"))
+       }
     })
   
+  output$data_column <- renderUI({
+    selectInput("column", strong("column of dataset:"), names(input_data()), selected = "Amount")
+  })
+  
+  records <- reactive({
+    input_data()[, input$column]
+  })
+  
   results_benford <- reactive({
-    benford(input_data(), input$digits)
-    })
+    benford(records(), input$digits)
+  })
   
   resul <- reactive({input$digit_click})
   
@@ -75,15 +87,15 @@ server <- function(input, output) {
   })
   
   output$plot_digits <- renderPlot({
-    plot(results_benford(), except=c("second order", "summation", "mantissa", "abs diff", "ex summation"), multiple=T)
+    plot(results_benford(), except = c("second order", "summation", "mantissa", "abs diff", "ex summation"), multiple=T)
   })
   
   output$plot_sec_ord <- renderPlot({
-    plot(results_benford(), except=c("digits", "summation", "mantissa","abs diff", "chi squared", "ex summation"), multiple=T)
+    plot(results_benford(), except = c("digits", "summation", "mantissa","abs diff", "chi squared", "ex summation"), multiple=T)
   })
   
   output$plot_summ_dist <- renderPlot({
-    plot(results_benford(), except=c("digits", "second order", "mantissa", "chi squared",  "ex summation"), multiple=T)
+    plot(results_benford(), except = c("digits", "second order", "mantissa", "chi squared",  "ex summation"), multiple=T)
   })
   
   output$results_benf <- renderPrint({
@@ -95,8 +107,8 @@ server <- function(input, output) {
   })
   
   output$suspect_records <- renderDataTable({
-    input_data <- extract.digits(input_data(), number.of.digits = input$digits)
-    getSuspects(results_benford(), input_data, how.many = input$ndigits)
+    records <- extract.digits(records(), number.of.digits = input$digits)
+    getSuspects(results_benford(), records, how.many = input$ndigits)
   })
   
   output$report <- downloadHandler(
