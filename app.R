ac815b2ea9492fb98b3c8a9db6dbf1fab8bfb314
Rafael da Silva Lins <rdasilvalins@gmail.com>
2019-08-21 00:26:17 -0300

    updates

--- app.R
+++ app.R
@@ -1,12 +1,12 @@
 
 library(shiny)
 library(shinythemes)
+# install the GitHub version
+# devtools::install_github("carloscinelli/benford.analysis", build_vignettes = TRUE)
 library(benford.analysis)
 library(knitr)
 library(dplyr)
 library(kableExtra)
-
-# see both graphs in the same panel
 
 ui <- navbarPage("Benford Analysis",
   theme = shinytheme("yeti"),
@@ -21,8 +21,6 @@ ui <- navbarPage("Benford Analysis",
                         "Negative" = "negative",
                         "Both" = "both"), inline = T),
          fluidRow(
-           #column(12, align = "center" , submitButton("Refresh"), br()),
-           #column(12, align = "center" , actionButton("reflesh", "Action button", class = "btn-primary")), br(),
            column(12, align = "center" , downloadButton("report", "Download report")))
       ),
       
@@ -33,15 +31,19 @@ ui <- navbarPage("Benford Analysis",
           tabPanel("Distribution of Digits",
                   column(12, br(), h5("The first graph concerns the observation count with respect to its first two digits, comparing it with the value expected by the Benford's Law.\n
                                       The second graph concerns the chi-squared statistics consisting of deviations of observed values from expected values.")),
-                   column(12, align = "center" ,
+                   column(10, align = "center" ,
                           plotOutput('plot_digits', click = "digit_click", height = "500px"),
+                          #verbatimTextOutput('tt'),
                           radioButtons("which_plot_digit", "",
                                        c("Digits" = "pdigit",
                                          "Rootogram" = "proot_digit",
                                          "Chi-Squared Difference" = "psquared"), inline = T),
-                          checkboxInput("option", "See all graphs in the same panel (não implementado)"),
                           dataTableOutput('verclick')
-                          )
+                          ),
+                  column(2, 
+                         'Plot Options',
+                         selectInput("col.digit.dist", strong("Color Plot Acoording:"), choices = c("Default", "Absolute Diff.", "Difference", "Chi-Squared")),
+                         checkboxInput("option", "See graphs in the same panel"))
                    ),
           tabPanel("Formal Tests",br(),
                    tabsetPanel(tabPanel("First Order Test",
@@ -73,7 +75,8 @@ ui <- navbarPage("Benford Analysis",
                                )
                   ),
           tabPanel("Suspect Records",
-                   numericInput("ndigits", strong('Number of suspicious groups:'), 3),
+                   numericInput("ndigits", strong('Number of suspicious groups:'), 3, min = 1),
+                   selectInput('metric', strong("Metric:"), choices = c("Absolute Difference", "Difference", "Chi-Squared", "Absolute Excess Summation")),
                    h5('leading digits by decreasing order of discrepancies (absolute differences)'),
                    column(12, align = "center" , tableOutput('leading_digits')),
                    h5("observations of the most suspicious groups with leading digits not complying with Benford's Law"),
@@ -110,24 +113,37 @@ server <- function(input, output) {
   results_benford <- reactive({
     benford(records(), input$digits, input$sign)
   })
-  
-  output$verclick <- renderDataTable({
+
+  d_click <- reactive({
     d1 <- min(results_benford()[["bfd"]]$digits)
     dn <- max(results_benford()[["bfd"]]$digits)
-    d_click <- round(d1 + (input$digit_click$x)*(dn - d1), 0)
-    getDigits(results_benford(), input_data(), d_click)
-    # kable(align = "c") %>%
-    # kable_styling(bootstrap_options = c("striped", "bordered", "hover"), full_width = F)
+    round(d1 + (input$digit_click$x)*(dn - d1)*1/(input$digit_click$domain$right - input$digit_click$domain$left), 0)
+  })
+  
+  output$verclick <- renderDataTable({
+    getDigits(results_benford(), input_data(), d_click())
   })
 
+  # col.bar <- reactive({
+  #   ndigts <- length(results_benford()[["bfd"]]$digits)
+  #   col.b <- rep("green", ndigts)
+  #   col.b[!(results_benford()[["bfd"]]$digits%in%d_click())] <- "lightblue"
+  #   col.b
+  # })
+  # 
+  # output$tt=renderText({
+  #   col.bar()
+  #  })
   
   output$plot_digits <- renderPlot({
+    ndigts <- length(results_benford()[["bfd"]]$digits)
+    col.b <- rep("green", ndigts)
+    col.b[!(results_benford()[["bfd"]]$digits%in%d_click())] <- "lightblue"
     switch(input$which_plot_digit,
-                   pdigit = plot(results_benford(), except = c("rootogram digits","second order","rootogram second order", "summation","chi squared", "mantissa", "abs diff", "ex summation", "legend"), multiple=F),
-                   proot_digit = plot(results_benford(), except = c("digits","second order", "rootogram second order", "summation","chi squared", "mantissa", "abs diff", "ex summation", "legend"), multiple=F),
+                   pdigit = plot(results_benford(), col.bar = col.b, except = c("rootogram digits","second order","rootogram second order", "summation","chi squared", "mantissa", "abs diff", "ex summation", "legend"), multiple=F),
+                   proot_digit = plot(results_benford(), col.bar = col.b,  except = c("digits","second order", "rootogram second order", "summation","chi squared", "mantissa", "abs diff", "ex summation", "legend"), multiple=F),
                    psquared = plot(results_benford(), except = c("digits","second order", "rootogram second order", "summation", "mantissa", "abs diff", "ex summation", "legend"), multiple=F))
   })
-  
 
   output$plot_sec_ord <- renderPlot({
     plot(results_benford(), except = c("digits","rootogram digits", "rootogram second order", "summation", "mantissa","abs diff", "chi squared", "ex summation", "legend"), multiple=T)
@@ -157,20 +173,28 @@ server <- function(input, output) {
   })
   
   output$leading_digits <- function(){
-      st <- head(suspectsTable(results_benford()), input$ndigits)
-      names(st) <- c("Digits", "Absolute Difference")
+    by.metric <- switch(input$metric,
+                        "Absolute Difference" = "absolute.diff",
+                        "Difference" = "difference",
+                        "Chi-Squared" = "squared.diff",
+                        "Absolute Excess Summation" = "abs.excess.summation")
+      st <- head(suspectsTable(results_benford(), by = by.metric), input$ndigits)
+      names(st) <- c("Digits", input$metric)
       st%>%
         kable(align = "c") %>%
         kable_styling(bootstrap_options = c("striped", "bordered", "hover"), full_width = F)
       
   }
-  
+
   output$suspect_records <-  renderDataTable({
     records <- extract.digits(records(), number.of.digits = input$digits)
-    sp <- getSuspects(results_benford(), input_data(), how.many = input$ndigits)
+    by.metric <- switch(input$metric,
+                        "Absolute Difference" = "absolute.diff",
+                        "Difference" = "difference",
+                        "Chi-Squared" = "squared.diff",
+                        "Absolute Excess Summation" = "abs.excess.summation")
+    sp <- getSuspects(results_benford(), input_data(), by = by.metric, how.many = input$ndigits)
     sp
-    # kable(align = "c") %>%
-    # kable_styling(bootstrap_options = c("striped", "bordered", "hover"), full_width = F)
   })
   
   output$report <- downloadHandler(
